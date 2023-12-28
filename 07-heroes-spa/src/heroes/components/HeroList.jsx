2dd6ff555563e0a644c343cb9f22ef158d8e39c1
Manuela <mabetancurgu@gmail.com>
2023-12-28 00:26:59 -0500

    Upgrade styles

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -5,7 +5,7 @@ import { useMemo } from "react";
 export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
-    <div className="row rows-cols-1 row-cols-md-3 g-3" style={{backgroundColor:'red'}}>
+    <div style={{ display: 'inline'}}>
       {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
